2d0561c06d01a3da055c0eef48bc5bbeca12fde6
sf44tdw <prmkaddr@yahoo.co.jp>
2014-10-18 21:29:20 +0900

    2

--- programme_channel_no.sql
+++ programme_channel_no.sql
@@ -8,3 +8,15 @@ programme.title,
 programme.start_datetime,
 programme.stop_datetime FROM channel,programme
 WHERE channel.channel_id=programme.channel_id and not exists (select * from paidBroadcasting where paidBroadcasting.channel_id=channel.channel_id);
+
+/*
+物理チャンネル読み替え用ビューから番組名を取得する。
+*_channel_no         :物理チャンネル番号
+*_start_datetime     :番組の開始時刻
+ */
+DELIMITER //
+CREATE PROCEDURE GET_TITLE(IN `_channel_no` int,IN `_start_datetime` DATETIME,)
+BEGIN
+SELECT `title` FROM `programme_channel_no` WHERE channel_no = _channel_no and start_datetime = _start_datetime;
+END//
+DELIMITER ;
