26bf2c291deb14b26a4ce7f0d83f713ec9722bcf
sf44tdw <prmkaddr@yahoo.co.jp>
2014-10-21 09:00:59 +0900

    4

--- programme_channel_no.sql
+++ programme_channel_no.sql
@@ -10,13 +10,13 @@ programme.stop_datetime FROM channel,programme
 WHERE channel.channel_id=programme.channel_id and not exists (select * from paidBroadcasting where paidBroadcasting.channel_id=channel.channel_id);
 
 /*
-物理チャンネル読み替え用ビューから番組名を取得する。
+物理チャンネル読み替え用ビューから指定されたチャンネルの、指定時刻から1分以内に始まる番組の番組名を取得する。
 *_channel_no         :物理チャンネル番号
 *_start_datetime     :番組の開始時刻
  */
 DELIMITER //
 CREATE PROCEDURE GET_TITLE(IN `_channel_no` int,IN `_start_datetime` DATETIME)
 BEGIN
-SELECT `title` FROM `programme_channel_no` WHERE channel_no = _channel_no and start_datetime = _start_datetime;
+SELECT `title` FROM `programme_channel_no` WHERE channel_no = _channel_no and start_datetime BETWEEN _start_datetime AND DATE_ADD(_start_datetime, INTERVAL 1 MINUTE);
 END//
 DELIMITER ;
