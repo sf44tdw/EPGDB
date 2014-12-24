fd5fe2d282291252e3c64f15317817da0053efea
sf44tdw <prmkaddr@yahoo.co.jp>
2014-12-25 04:40:35 +0900

    ストアドプロシージャの間違いを修正

--- programme_channel_no.sql
+++ programme_channel_no.sql
@@ -24,14 +24,15 @@ DELIMITER ;
 
 
 /*
-物理チャンネル読み替え用ビューから指定されたチャンネルの、指定時刻に始まる番組の番組名を取得する。(2件以上あるかもしれない)
+物理チャンネル読み替え用ビューから指定されたチャンネルの、指定時刻に始まる番組の番組情報を取得する。(2件以上あるかもしれない)
+*_channel_id:チャンネルID
 *_channel_no         :物理チャンネル番号
 *_start_datetime     :番組の開始時刻
  */
 DELIMITER //
-CREATE PROCEDURE GET_PROGRAMME_FUZZY(IN `_channel_no` int,IN `_start_datetime` DATETIME)
+CREATE PROCEDURE GET_PROGRAMME_FUZZY(IN `_channel_id` char(7), IN `_channel_no` int,IN `_start_datetime` DATETIME)
 BEGIN
-SELECT `title` FROM `programme_channel_no` WHERE channel_no = _channel_no and start_datetime = _start_datetime;
+SELECT * FROM `programme_channel_no` WHERE channel_id=_channel_id and  channel_no = _channel_no and start_datetime = _start_datetime;
 END//
 DELIMITER ;
 
@@ -39,12 +40,13 @@ DELIMITER ;
 /*
 物理チャンネル読み替え用ビューから指定された1件の番組の情報を取得する。(参照元のテーブルの制約により、2件以上の番組情報が取得されることはありえない)
 *_channel_id:チャンネルID
+*_channel_no         :物理チャンネル番号
 *_event_id  :番組ID
 *_start_datetime     :番組の開始時刻
  */
 DELIMITER //
-CREATE PROCEDURE GET_PROGRAMME_STRICT(IN `_channel_id` char(7),IN `_event_id` int,IN `_start_datetime` DATETIME)
+CREATE PROCEDURE GET_PROGRAMME_STRICT(IN `_channel_id` char(7), IN `_channel_no` int,IN `_event_id` int,IN `_start_datetime` DATETIME)
 BEGIN
-SELECT * FROM `programme_channel_no` WHERE channel_id = _channel_id and event_id=_event_id and start_datetime=_start_datetime;
+SELECT * FROM `programme_channel_no` WHERE channel_id = _channel_id and channel_no=_channel_no and event_id=_event_id and start_datetime=_start_datetime;
 END//
 DELIMITER ;
