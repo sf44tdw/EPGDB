8e3a671f00c708f3610b6380c0eed3641f4ce26e
sf44tdw <prmkaddr@yahoo.co.jp>
2014-12-24 21:50:55 +0900

    番組情報取得用プロシージャ変更。
    3時間以内の一覧取得を削除。
    特定の番組情報取得用プロシージャを追加。

--- programme_channel_no.sql
+++ programme_channel_no.sql
@@ -22,14 +22,29 @@ SELECT `title` FROM `programme_channel_no` WHERE channel_no = _channel_no and st
 END//
 DELIMITER ;
 
+
 /*
-物理チャンネル読み替え用ビューから指定されたチャンネルの、指定時刻から180分以内に始まる番組情報を取得する。
+物理チャンネル読み替え用ビューから指定されたチャンネルの、指定時刻に始まる番組の番組名を取得する。(2件以上あるかもしれない)
 *_channel_no         :物理チャンネル番号
-*_start_datetime     :放送開始時刻の検索範囲の始点となる時刻
+*_start_datetime     :番組の開始時刻
  */
 DELIMITER //
-CREATE PROCEDURE GET_PROGRAMMES_AFTER_3_HOUR(IN `_channel_no` int,IN `_start_datetime` DATETIME)
+CREATE PROCEDURE GET_PROGRAMME_FUZZY(IN `_channel_no` int,IN `_start_datetime` DATETIME)
 BEGIN
-SELECT * FROM `programme_channel_no` WHERE channel_no = _channel_no and start_datetime BETWEEN  _start_datetime AND DATE_ADD(_start_datetime, INTERVAL 3 HOUR);
+SELECT `title` FROM `programme_channel_no` WHERE channel_no = _channel_no and start_datetime = _start_datetime;
+END//
+DELIMITER ;
+
+
+/*
+物理チャンネル読み替え用ビューから指定された1件の番組の情報を取得する。(参照元のテーブルの制約により、2件以上の番組情報が取得されることはありえない)
+*_channel_id:チャンネルID
+*_event_id  :番組ID
+*_start_datetime     :番組の開始時刻
+ */
+DELIMITER //
+CREATE PROCEDURE GET_PROGRAMME_STRICT(IN `_channel_id` char(7),IN `_event_id` int,IN `_start_datetime` DATETIME)
+BEGIN
+SELECT * FROM `programme_channel_no` WHERE channel_id = _channel_id and event_id=_event_id and start_datetime=_start_datetime;
 END//
 DELIMITER ;
