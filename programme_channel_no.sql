d2485d7b5e5a0f99b1a631f180b02420f2845d24
sf44tdw <prmkaddr@yahoo.co.jp>
2014-12-24 18:12:13 +0900

    特定の時間から3時間以内に始まる番組を検索するプロシージャを追加。

--- programme_channel_no.sql
+++ programme_channel_no.sql
@@ -4,6 +4,7 @@
 CREATE SQL SECURITY INVOKER VIEW programme_channel_no AS 
 SELECT channel.channel_id,
 channel.channel_no,
+programme.event_id,
 programme.title,
 programme.start_datetime,
 programme.stop_datetime FROM channel,programme
@@ -18,5 +19,17 @@ DELIMITER //
 CREATE PROCEDURE GET_TITLE(IN `_channel_no` int,IN `_start_datetime` DATETIME)
 BEGIN
 SELECT `title` FROM `programme_channel_no` WHERE channel_no = _channel_no and start_datetime BETWEEN _start_datetime AND DATE_ADD(_start_datetime, INTERVAL 1 MINUTE);
+END//
+DELIMITER ;
+
+/*
+物理チャンネル読み替え用ビューから指定されたチャンネルの、指定時刻から180分以内に始まる番組情報を取得する。
+*_channel_no         :物理チャンネル番号
+*_start_datetime     :放送開始時刻の検索範囲の始点となる時刻
+ */
+DELIMITER //
+CREATE PROCEDURE GET_PROGRAMMES_AFTER_3_HOUR(IN `_channel_no` int,IN `_start_datetime` DATETIME)
+BEGIN
+SELECT * FROM `programme_channel_no` WHERE channel_no = _channel_no and start_datetime BETWEEN  _start_datetime AND DATE_ADD(_start_datetime, INTERVAL 3 HOUR);
 END//
 DELIMITER ;
