b8b457c075202619cb106243586dc1a0bb0fa32b
sf44tdw <prmkaddr@yahoo.co.jp>
2015-07-24 04:39:06 +0900

    コメント修正。チャンネルテーブルの制約をテーブル定義に統合。

--- channel.sql
+++ channel.sql
@@ -1,20 +1,18 @@
 /* チャンネル一覧テーブル。チャンネルの読み替え用。
-channel_id  :EPGに書かれていたチャンネルID
-channel_no  :対応する物理チャンネル番号(例:東京タワーからのnhk総合なら27)
-display_name:EPGに書かれていた局名
+channel_id      :EPGに書かれていたチャンネルID
+channel_no      :対応する物理チャンネル番号(例:東京タワーからのnhk総合なら27)
+display_name    :EPGに書かれていた局名
 insert_datetime :この情報の追加日時
+チャンネルIDと物理チャンネル番号の両方が等しいレコードが2件以上存在することは禁止する。
  */
 CREATE TABLE `channel` (
-  `channel_id` VARCHAR(20) NOT NULL,
-  `channel_no` int NOT NULL,
+  `channel_id` varchar(20) NOT NULL,
+  `channel_no` int(11) NOT NULL,
   `display_name` text,
-  `insert_datetime` DATETIME NOT NULL,
-  PRIMARY KEY (`channel_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/* チャンネルIDと物理番号が等しい組み合わせを明示的に禁止(チャンネルIDがユニークなので別に要らない)
- */
-ALTER TABLE `channel` ADD UNIQUE ch_id_no(`channel_id`, `channel_no`);
+  `insert_datetime` datetime NOT NULL,
+  PRIMARY KEY (`channel_id`),
+  UNIQUE KEY `ch_id_no` (`channel_id`,`channel_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
 /* チャンネル一覧テーブルに内容を追加する。追加日時は自動入力する。
  */
