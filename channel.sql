fedf270ccba4be7d9a68b18e2c69f265ca261704
sf44tdw <prmkaddr@yahoo.co.jp>
2017-09-03 13:24:03 +0900

    ビュー、ストアドの削除。JPAに移行。

--- channel.sql
+++ channel.sql
@@ -2,63 +2,13 @@
 CHANNEL_ID      :EPGに書かれていたチャンネルID
 CHANNEL_NO      :対応する物理チャンネル番号(例:東京タワーからのNHK総合なら27)
 DISPLAY_NAME    :EPGに書かれていた局名
-INSERT_DATETIME :この情報の追加日時(自動入力)
 チャンネルIDと物理チャンネル番号の両方が等しいレコードが2件以上存在することは禁止する。
  */
 CREATE TABLE `CHANNEL` (
   `CHANNEL_ID` VARCHAR(20) NOT NULL,
   `CHANNEL_NO` INT(11) NOT NULL,
   `DISPLAY_NAME` TEXT,
-  `INSERT_DATETIME` TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
   PRIMARY KEY (`CHANNEL_ID`),
   UNIQUE KEY `CH_ID_NO` (`CHANNEL_ID`,`CHANNEL_NO`)
 ) ENGINE=INNODB DEFAULT CHARSET=UTF8;
 
-/* チャンネル一覧テーブルに内容を追加する。追加日時は自動入力する。
- */
-INSERT INTO `CHANNEL` (`CHANNEL_ID`,`CHANNEL_NO`,`DISPLAY_NAME`) VALUES (`_CHANNEL_ID`,`_CHANNEL_NO`,`_DISPLAY_NAME`);
-
-
-/*
-INSERT INTO `CHANNEL` (`CHANNEL_ID`,`CHANNEL_NO`,`DISPLAY_NAME`) VALUES ("ID1",0,"TV1");
-INSERT INTO `CHANNEL` (`CHANNEL_ID`,`CHANNEL_NO`,`DISPLAY_NAME`) VALUES ("ID2",1,"TV2");
-INSERT INTO `CHANNEL` (`CHANNEL_ID`,`CHANNEL_NO`,`DISPLAY_NAME`) VALUES ("ID3",2,"TV3");
-INSERT INTO `CHANNEL` (`CHANNEL_ID`,`CHANNEL_NO`,`DISPLAY_NAME`) VALUES ("ID4-1",3,"TV4-1");
-INSERT INTO `CHANNEL` (`CHANNEL_ID`,`CHANNEL_NO`,`DISPLAY_NAME`) VALUES ("ID4-2",3,"TV4-2");
-SELECT * FROM CHANNEL;
-
-
-
-mysql> INSERT INTO `CHANNEL` (`CHANNEL_ID`,`CHANNEL_NO`,`DISPLAY_NAME`) VALUES ("ID1",0,"TV1");
-INSERT INTO `CHANNEL` (`CHANNEL_ID`,`CHANNEL_NO`,`DISPLAY_NAME`) VALUES ("ID2",1,"TV2");
-INSERT INTO `CHANNEL` (`CHANNEL_ID`,`CHANNEL_NO`,`DISPLAY_NAME`) VALUES ("ID3",2,"TV3");
-Query OK, 1 row affected (0.04 sec)
-
-mysql> INSERT INTO `CHANNEL` (`CHANNEL_ID`,`CHANNEL_NO`,`DISPLAY_NAME`) VALUES ("ID2",1,"TV2");
-INSERT INTO `CHANNEL` (`CHANNEL_ID`,`CHANNEL_NO`,`DISPLAY_NAME`) VALUES ("ID4-1",3,"TV4-1");
-INSERT INTO `CHANNEL` (`CHANNEL_ID`,`CHANNEL_NO`,`DISPLAY_NAME`) VALUES ("ID4-2",3,"TV4-2");
-Query OK, 1 row affected (0.02 sec)
-
-mysql> INSERT INTO `CHANNEL` (`CHANNEL_ID`,`CHANNEL_NO`,`DISPLAY_NAME`) VALUES ("ID3",2,"TV3");
-SELECT * FROM CHANNEL;Query OK, 1 row affected (0.03 sec)
-
-mysql> INSERT INTO `CHANNEL` (`CHANNEL_ID`,`CHANNEL_NO`,`DISPLAY_NAME`) VALUES ("ID4-1",3,"TV4-1");
-Query OK, 1 row affected (0.02 sec)
-
-mysql> INSERT INTO `CHANNEL` (`CHANNEL_ID`,`CHANNEL_NO`,`DISPLAY_NAME`) VALUES ("ID4-2",3,"TV4-2");
-Query OK, 1 row affected (0.04 sec)
-
-mysql> SELECT * FROM CHANNEL;
-+------------+------------+--------------+---------------------+
-| CHANNEL_ID | CHANNEL_NO | DISPLAY_NAME | INSERT_DATETIME     |
-+------------+------------+--------------+---------------------+
-| ID1        |          0 | TV1          | 2017-03-12 16:39:07 |
-| ID2        |          1 | TV2          | 2017-03-12 16:39:07 |
-| ID3        |          2 | TV3          | 2017-03-12 16:39:07 |
-| ID4-1      |          3 | TV4-1        | 2017-03-12 16:39:07 |
-| ID4-2      |          3 | TV4-2        | 2017-03-12 16:39:07 |
-+------------+------------+--------------+---------------------+
-5 rows in set (0.00 sec)
-
-mysql>
-*/
--- programme.sql
+++ programme.sql
@@ -4,10 +4,9 @@ ID              :主キー。自動で生成され、加算される。
 EVENT_ID        :番組ID(しょっちゅう再利用されているから主キーにはしない)
 CHANNEL         :チャンネルID(チャンネル一覧テーブルから参照させて物理チャンネルを確認する。)
 TITLE           :タイトル
-START           :開始時刻(インデックスつき)
-STOP            :終了時刻
-INSERT_DATETIME :この情報の追加日時(自動入力)
-番組ID,チャンネルID,開始時刻の全てが同じデータは登録できない。
+START_DATETIME           :開始時刻(インデックスつき)
+STOP_DATETIME            :終了時刻(インデックスつき)
+番組ID,チャンネルIDが同じデータは登録できない。
  */
 CREATE TABLE `PROGRAMME` (
   `ID` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -16,96 +15,10 @@ CREATE TABLE `PROGRAMME` (
   `TITLE` TEXT NOT NULL,
   `START_DATETIME` DATETIME NOT NULL,
   `STOP_DATETIME` DATETIME NOT NULL,
-  `INSERT_DATETIME` TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
   PRIMARY KEY (`ID`),
-  UNIQUE KEY `CH_ID_NO` (`CHANNEL_ID`,`EVENT_ID`,`START_DATETIME`),
+  UNIQUE KEY `CH_ID_NO` (`CHANNEL_ID`,`EVENT_ID`),
   KEY `CHANNEL_ID` (`CHANNEL_ID`),
   KEY `START_DATETIME_I` (`START_DATETIME`),
+  KEY `STOP_DATETIME_I` (`STOP_DATETIME`),
   CONSTRAINT `PROGRAMME_IBFK_1` FOREIGN KEY (`CHANNEL_ID`) REFERENCES `CHANNEL` (`CHANNEL_ID`)
 ) ENGINE=INNODB;
-
-/*
-MYSQLではCHECK制約が使えないのでトリガーで代用する。
-レコードの追加、更新の際に、放送開始時刻が放送終了時刻以前になっていたら、
-追加、更新しようとしているレコードのNULLにしてはいけないフィールド(CHANNEL_ID)にNULLを設定し、ERROR 1048 (23000): COLUMN 'CHANNEL_ID' CANNOT BE NULLを発生させることで、問題のあるレコードが作られないようにする。
-タブを入れると"DISPLAY ALL XXX POSSIBILITIES? (Y OR N)"と出てくるのでタブを除去。
-SIGNALは、MYSQL5.1では使えない。
- */
-DELIMITER $$
-
-CREATE TRIGGER UPDATE_PROGRAMME BEFORE UPDATE ON `PROGRAMME` FOR EACH ROW
-BEGIN
-IF NEW.`START_DATETIME` >= NEW.`STOP_DATETIME` THEN
-SET NEW.`CHANNEL_ID` = NULL;
-END IF;
-END;$$
-
-
-
-CREATE TRIGGER INSERT_PROGRAMME BEFORE INSERT ON `PROGRAMME` FOR EACH ROW
-BEGIN
-IF NEW.`START_DATETIME` >= NEW.`STOP_DATETIME` THEN
-SET NEW.`CHANNEL_ID` = NULL;
-END IF;
-END;$$
-
-DELIMITER ;
-
-/*
-SELECT * FROM PROGRAMME;
-INSERT INTO `PROGRAMME` (`CHANNEL_ID`,`EVENT_ID`,`TITLE`,`START_DATETIME`,`STOP_DATETIME`) VALUES ("ID2",100,"DUMMY_TITLE","2015-08-10 10:00:00","2015-08-10 10:00:00");
-INSERT INTO `PROGRAMME` (`CHANNEL_ID`,`EVENT_ID`,`TITLE`,`START_DATETIME`,`STOP_DATETIME`) VALUES ("ID2",100,"DUMMY_TITLE","2015-08-10 10:00:00","2015-08-10 09:00:00");
-INSERT INTO `PROGRAMME` (`CHANNEL_ID`,`EVENT_ID`,`TITLE`,`START_DATETIME`,`STOP_DATETIME`) VALUES ("ID2",100,"DUMMY_TITLE","2015-08-10 10:00:00","2015-08-10 11:00:00");
-SELECT * FROM PROGRAMME;
-
-UPDATE PROGRAMME SET START_DATETIME="2015-08-20 10:00:00" WHERE EVENT_ID=100;
-UPDATE PROGRAMME SET START_DATETIME="2015-08-03 10:00:00" WHERE EVENT_ID=100;
-SELECT * FROM PROGRAMME;
-*/
-
-/*
-mysql> SELECT * FROM PROGRAMME;
-Empty set (0.00 sec)
-
-mysql> INSERT INTO `PROGRAMME` (`CHANNEL_ID`,`EVENT_ID`,`TITLE`,`START_DATETIME`,`STOP_DATETIME`) VALUES ("ID1",100,"DUMMY_TITLE","2015-08-10 10:00:00","2015-08-10 10:00:00");
-ERROR 1048 (23000): Column 'CHANNEL_ID' cannot be null
-mysql> INSERT INTO `PROGRAMME` (`CHANNEL_ID`,`EVENT_ID`,`TITLE`,`START_DATETIME`,`STOP_DATETIME`) VALUES ("ID1",100,"DUMMY_TITLE","2015-08-10 10:00:00","2015-08-10 09:00:00");
-ERROR 1048 (23000): Column 'CHANNEL_ID' cannot be null
-mysql> INSERT INTO `PROGRAMME` (`CHANNEL_ID`,`EVENT_ID`,`TITLE`,`START_DATETIME`,`STOP_DATETIME`) VALUES ("ID1",100,"DUMMY_TITLE","2015-08-10 10:00:00","2015-08-10 11:00:00");
-SELECT * FROM PROGRAMME;
-Query OK, 1 row affected (0.02 sec)
-
-mysql> SELECT * FROM PROGRAMME;
-+----+------------+----------+-------------+---------------------+---------------------+---------------------+
-| ID | CHANNEL_ID | EVENT_ID | TITLE       | START_DATETIME      | STOP_DATETIME       | INSERT_DATETIME     |
-+----+------------+----------+-------------+---------------------+---------------------+---------------------+
-|  1 | ID1        |      100 | DUMMY_TITLE | 2015-08-10 10:00:00 | 2015-08-10 11:00:00 | 2017-03-12 16:40:47 |
-+----+------------+----------+-------------+---------------------+---------------------+---------------------+
-1 row in set (0.00 sec)
-
-mysql>
-mysql> UPDATE PROGRAMME SET START_DATETIME="2015-08-20 10:00:00" WHERE EVENT_ID=100;
-ERROR 1048 (23000): Column 'CHANNEL_ID' cannot be null
-mysql> UPDATE PROGRAMME SET START_DATETIME="2015-08-03 10:00:00" WHERE EVENT_ID=100;
-SELECT * FROM PROGRAMME;Query OK, 1 row affected (0.02 sec)
-Rows matched: 1  Changed: 1  Warnings: 0
-
-mysql> SELECT * FROM PROGRAMME;
-+----+------------+----------+-------------+---------------------+---------------------+---------------------+
-| ID | CHANNEL_ID | EVENT_ID | TITLE       | START_DATETIME      | STOP_DATETIME       | INSERT_DATETIME     |
-+----+------------+----------+-------------+---------------------+---------------------+---------------------+
-|  1 | ID1        |      100 | DUMMY_TITLE | 2015-08-03 10:00:00 | 2015-08-10 11:00:00 | 2017-03-12 16:40:47 |
-+----+------------+----------+-------------+---------------------+---------------------+---------------------+
-1 row in set (0.00 sec)
-
-mysql>
-*/
-
-
-
-/*
-番組情報テーブルに内容を追加する。追加日時は自動入力する。
-チャンネルテーブルにチャンネルIDが登録されていない場合、参照整合性制約により登録できないことを前提とする。
-SQLEXCEPTION E.GETSQLSTATE()でエラー番号はわかるのでそうする。
- */
-INSERT INTO `PROGRAMME` (`CHANNEL_ID`,`EVENT_ID`,`TITLE`,`START_DATETIME`,`STOP_DATETIME`) VALUES (`_CHANNEL_ID`,`_EVENT_ID`,`_TITLE`,`_START_DATETIME`,`_STOP_DATETIME`);
